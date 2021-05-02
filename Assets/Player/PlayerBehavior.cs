145f232f1e15a28d1d953cb0c431de6d0202fd07
Doug Parker <dgp1130@users.noreply.github.com>
2021-05-01 18:45:22 -0700

    Adds player sprite and basic movement.
    WASD allows the character to move, though currently we don't update the sprite to reflect this.
    
    Spritesheet courtesy of https://www.pngjoy.com/fullpng/x7k8g3q0u1s1x1/.

---
+++ Assets/Player/PlayerBehavior.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector2 moveVec = Vector2.zero;
+    [SerializeField] float speed = 1.0f;
+
+    public void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + (moveVec * Time.deltaTime * speed));
+    }
+
+    public void OnMove(InputValue input)
+    {
+        moveVec = input.Get<Vector2>();
+    }
+}
