10e6877a7357a85c208c9adafcd7ea2f4bd2d033
Doug Parker <dgp1130@users.noreply.github.com>
2021-05-22 21:01:53 -0700

    Adds a rock which can only be destroyed via bombs.
    The rock is a simple prefab with a hurtbox. Its most interesting trait is that it's defense is immune to base damage, meaning it is only vulnerable to explosive damage from bombs. This required a new `ImmuneResistence` which drops all received damage. Fortunately it has no parameters so it can easily be reused across multiple objects.

---
+++ Assets/Combat/ImmuneResistence.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/** Resistence which blocks all incoming damage from the associated type. */
+[CreateAssetMenu(menuName = "LinkToTheRandomizer/ImmuneResistence")]
+public sealed class ImmuneResistence : Resistence
+{
+    public override int Resist(int damage)
+    {
+        // Immune to all damage of this type.
+        return 0;
+    }
+}
