9abdba7ed349f3ca8b1a1babdef2f673bfa48f46
Doug Parker <dgp1130@users.noreply.github.com>
2021-05-24 16:34:57 -0700

    Always advances the title screen after 3 seconds.
    This speeds up the auto-advance and removes the player's ability to press a key to move forward. Reasoning for this is because when the game ends, the player may be moving around and pressing buttons, which would immediately skip the title after it shows but before the player has a chance to see it. This is just the simplest solution to that problem.

--- Assets/Scenes/Title/TitleStart.cs
+++ Assets/Scenes/Title/TitleStart.cs
@@ -6,27 +6,15 @@ using UnityEngine.SceneManagement;
 
 public sealed class TitleStart : MonoBehaviour
 {
-    private IEnumerator autoStarter = null!;
-
     private void Awake()
     {
-        autoStarter = autoStart();
-        StartCoroutine(autoStarter);
+        StartCoroutine(autoStart());
     }
 
     /** Automatically start the game in 5 seconds. */
     private IEnumerator autoStart()
     {
-        yield return new WaitForSeconds(5);
-        startGame();
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Instructions");
     }
-
-    /** Called when the user presses any key on the title screen. */
-    private void OnStartGame()
-    {
-        StopCoroutine(autoStarter);
-        startGame();
-    }
-
-    private void startGame() => SceneManager.LoadScene("Instructions");
 }
