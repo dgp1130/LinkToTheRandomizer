dfca6c81f69c756ec8976260c0ad53113aef96db
Doug Parker <dgp1130@users.noreply.github.com>
2021-05-24 15:58:53 -0700

    Automatically starts the game after 5 seconds on the title screen.
    This is for users who might not realize they need to press a button, on those on mobile who can't progress without a keypress and won't know that until they reach the instructions screen.

--- Assets/Scenes/Title/TitleStart.cs
+++ Assets/Scenes/Title/TitleStart.cs
@@ -1,13 +1,32 @@
 #nullable enable
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public sealed class TitleStart : MonoBehaviour
 {
+    private IEnumerator autoStarter = null!;
+
+    private void Awake()
+    {
+        autoStarter = autoStart();
+        StartCoroutine(autoStarter);
+    }
+
+    /** Automatically start the game in 5 seconds. */
+    private IEnumerator autoStart()
+    {
+        yield return new WaitForSeconds(5);
+        startGame();
+    }
+
     /** Called when the user presses any key on the title screen. */
     private void OnStartGame()
     {
-        SceneManager.LoadScene("Instructions");
+        StopCoroutine(autoStarter);
+        startGame();
     }
+
+    private void startGame() => SceneManager.LoadScene("Instructions");
 }
